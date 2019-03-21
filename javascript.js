8a018f4646907bbfc356b58401ee44c42ab33f12
Blue-Moon <moon7392@naver.com>
2019-03-21 19:49:31 +0900

    Let me introduce

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+var bt=document.getElementById('bt');
+
+	bt.addEventListener('click',function(){
+		alert("전공은은 전자공학입니다.");
+	})
+
+var bt2=document.getElementById('bt2');
+	bt2.addEventListener('click',function(){
+		alert("복수전공은 컴퓨터소프트웨어학과입니다.");
+	})
